d178300e92ef8c431ce36f44ee31b621f9dc441c
Luis Mancilla Avila <luis.mancilla.avila@gmail.com>
2014-02-20 17:28:49 -0300

    version 4.6.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default[:tomcat][:user][:username] = "admin"
 default[:tomcat][:user][:password] = "4dm1n"
 
-default[:solr][:version]     = "4.2.1"
+default[:solr][:version]     = "4.6.1"
 default[:solr][:url]         = "http://mirror.sdunix.com/apache/lucene/solr/#{node[:solr][:version]}/solr-#{node[:solr][:version]}.tgz"
 default[:solr][:unpack_path] = "/tmp/apache-solr-#{node[:solr][:version]}"
